87a94aa482341f639fc6d2cb98b0c92e77becab6
Gulati <Nitya.Gulati@surescripts.com>
2023-05-10 22:57:46 -0400

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <ContentCard></ContentCard>
+  <Footer></Footer>
 </template>
 
 <script>
 import ContentCard from './components/ContentCard.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    ContentCard
+    ContentCard,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer">
+        <p>Challenge by <a href="https://www.frontendmentor.io" target="_blank">Frontend Mentor</a> 🤍 Coded by <a href="https://www.nityagulati.com" target="_blank">Nitya Gulati</a>.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+.footer {
+    position: absolute;
+    bottom: 20px;
+}
+
+a {
+    color: var(--color-text);
+}
+
+a:hover {
+    color: var(--color-accent-muted);
+}
+</style>
