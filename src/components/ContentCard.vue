c8099777336c848672a5f297d6b114e3579c39bc
Gulati <Nitya.Gulati@surescripts.com>
2023-05-10 20:45:42 -0400

    update styles

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -2,7 +2,7 @@
 <div class="card shadow">
     <p class="card-label">Advice #</p>
     <p class="card-quote">“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p>
-    <img class="pattern" src="" alt="">
+    <div class="pattern"></div>
     <div class="dice-wrap">
         <img class="dice" src="../assets/images/icon-dice.svg" alt="Click to switch quote">
     </div>
@@ -18,10 +18,15 @@ export default {
 <style>
 .card {
     width: 250px;
-    height: 330px;
+    height: auto;
+    min-height: 330px;
     background: var(--color-card-bg);
     border-radius: 15px;
     padding: 40px 14px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 .card-label {
@@ -36,8 +41,11 @@ export default {
 }
 
 .pattern {
-    content: url('../assets/images/pattern-divider-mobile.svg');
-    zoom: 65%;
+    content: '';
+    background: url('../assets/images/pattern-divider-mobile.svg') center no-repeat;
+    width: 100%;
+    height: 16px;
+    margin-bottom: 30px;
 }
 
 .dice-wrap {
@@ -46,8 +54,7 @@ export default {
     height: 64px;
     border-radius: 50%;
     position: absolute;
-    left: 93px;
-    top: 298px;
+    bottom: -30px;
     cursor: pointer;
 }
 
@@ -66,24 +73,17 @@ export default {
 @media screen and (min-width: 375px) {
     .card {
         width: 343px;
-        height: 315px;
+        height: auto;
+        min-height: 315px;
         padding: 40px 24px;
-    }
-
-    .pattern {
-        zoom: 1;
-    }
-
-    .dice-wrap {
-        left: 139px;
-        top: 283px;
     }
 }
 
 @media screen and (min-width: 768px) {
     .card {
         width: 540px;
-        height: 332px;
+        height: auto;
+        min-height: 332px;
         padding: 48px;
     }
 
@@ -97,12 +97,8 @@ export default {
     }
 
     .pattern {
-        content: url('../assets/images/pattern-divider-desktop.svg')
-    }
-
-    .dice-wrap {
-        left: 238px;
-        top: 300px;
+        content: '';
+        background: url('../assets/images/pattern-divider-desktop.svg') center no-repeat;
     }
 }
 </style>
